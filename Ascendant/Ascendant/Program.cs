d4d588d2dfe0400faa442789d11ef2b0095e89fa
John Pavlik <john.a.pavlik@gmail.com>
2015-06-18 20:39:18 -0400

    Testing OpenGL

--- Ascendant/Ascendant/Program.cs
+++ Ascendant/Ascendant/Program.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
-namespace Ascendant
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello world!");
-        }
+using Ascendant.Graphics;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+namespace Ascendant {
+  class Program {
+    static void Main(string[] args) {
+      using(var window = new Window()) {
+        window.Run(60);
+      }
     }
+  }
 }
